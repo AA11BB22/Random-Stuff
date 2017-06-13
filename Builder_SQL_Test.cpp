d89ba28d09106ec2075d6a9d580e416b71e7c0dc
Choe Choon Ho <choechoonho@gmail.com>
2017-06-13 18:05:55 +0800

    Add files via upload
    Builder on SQL + variadic template function.

---
+++ Builder_SQL_Test.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#include <assert.h>
+
+using namespace std;
+
+class Database
+{
+public:
+    class SQL;
+    
+    void ResetQuery() { m_szQuery = ""; }
+    string GetQuery() const { return m_szQuery; }
+    
+private:
+    string m_szQuery;
+    
+    Database(string Query) : m_szQuery(Query) { }
+};
+
+static const string OR = " OR ";
+static const string AND = " AND ";
+
+class Database::SQL
+{
+public:
+    SQL() { }
+    Database EndSQL();
+    
+    template<class T>
+    SQL SELECT(T tail)
+    {
+        m_szQuery += (string) tail;
+        return *this;
+    }
+    
+    template<class T, class... Ts>
+    SQL SELECT(T head, Ts... tail)
+    {
+        if (m_szQuery.length() == 0) m_szQuery += "SELECT ";
+        m_szQuery += ((string) head + ", ");
+        return SELECT(tail...);
+    }
+    
+    SQL FROM(const string &szDatabase)
+    {
+        m_szQuery += (" FROM " + szDatabase);
+        return *this;
+    }
+    
+    SQL WHERE()
+    {
+        m_szQuery += " WHERE ";
+        return *this;
+    }
+    
+    SQL StartOR();
+    SQL EndOR();
+    
+    SQL StartAND();
+    SQL EndAND();
+    
+    SQL EQUAL(const string &szFieldName, const string &szValue);
+
+private:
+    string m_szQuery;
+    stack<string> m_Operations;
+};
+
+Database::SQL Database::SQL::StartOR()
+{
+    if (m_Operations.size()) m_szQuery += m_Operations.top();
+    
+    m_szQuery += '(';
+    m_Operations.push(OR);
+    return *this;
+}
+
+Database::SQL Database::SQL::EndOR()
+{
+    assert(m_Operations.size() && m_Operations.top() == OR && m_szQuery.length() && m_szQuery[m_szQuery.length() - 1] != '(');
+    
+    m_szQuery += ')';
+    m_Operations.pop();
+    return *this;
+}
+
+Database::SQL Database::SQL::StartAND()
+{
+    if (m_Operations.size()) m_szQuery += m_Operations.top();
+    
+    m_szQuery += '(';
+    m_Operations.push(AND);
+    return *this;
+}
+
+Database::SQL Database::SQL::EndAND()
+{
+    assert(m_Operations.size() && m_Operations.top() == AND && m_szQuery.length() && m_szQuery[m_szQuery.length() - 1] != '(');
+    
+    m_szQuery += ')';
+    m_Operations.pop();
+    return *this;
+}
+
+Database::SQL Database::SQL::EQUAL(const string &szFieldName, const string &szValue)
+{
+    if (m_Operations.size() && m_szQuery.length() && m_szQuery[m_szQuery.length() - 1] != '(') m_szQuery += m_Operations.top();
+    m_szQuery += (szFieldName + " = \'" + szValue + '\'');
+    return *this;
+}
+
+Database Database::SQL::EndSQL()
+{
+    assert(m_Operations.size() == 0);
+    
+    return Database(m_szQuery);
+}
+
+int main()
+{
+    Database Db = Database::
+                    SQL().SELECT("A", "B", "C", "D").FROM("ABC").WHERE().
+                        StartOR().
+                            EQUAL("column 1", "value").
+                            EQUAL("column 2", "value").
+                            StartAND().
+                                EQUAL("column 3", "value").
+                                EQUAL("column 4", "value").
+                            EndAND().
+                            StartOR().
+                                EQUAL("column 5", "value").
+                                StartAND().
+                                    EQUAL("column 6", "value").
+                                    EQUAL("column 7", "value").
+                                EndAND().
+                            EndOR().
+                        EndOR().
+                    EndSQL();
+    
+    string Query = Db.GetQuery();
+    cout << Query << endl;
+    
+    return 1;
+}
